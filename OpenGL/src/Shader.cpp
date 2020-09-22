a7ef5b07ca9ec81453ecc1bfb80409feb27f9f14
CosmicPotato <54864515+msunde137@users.noreply.github.com>
2020-09-22 11:07:02 -0400

    imgui implementation

--- OpenGL/src/Shader.cpp
+++ OpenGL/src/Shader.cpp
@@ -123,6 +123,11 @@ void Shader::SetUniform1i(const std::string& name, int i)
     GLCall(glUniform1i(GetUniformLocation(name), i));
 }
 
+void Shader::SetUniformMat4f(const std::string& name, glm::mat4& matrix)
+{
+    GLCall(glUniformMatrix4fv(GetUniformLocation(name), 1, GL_FALSE, &matrix[0][0]));
+}
+
 int Shader::GetUniformLocation(const std::string& name)
 {
     if (m_UniformLocationCache.find(name) != m_UniformLocationCache.end())
